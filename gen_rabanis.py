Refactor for future dev, prevent overflow error on input=0, fix incorrect calling and early stopping

gen_rabanis.py
@@ -3,7 +3,7 @@ import re
 import time
 from datetime import datetime
 from os import listdir
-
+import h5py
 import numpy as np
 from matplotlib import colors
 from numba import prange, jit
@@ -11,23 +11,58 @@ from numba import prange, jit
 from rabani import rabani_single
 
 
-@jit(nopython=True, parallel=True)
-def gen_rabani_sweep(kT_min, kT_max, mu_min, mu_max, axis_steps):
-    runs = np.zeros((128, 128, axis_steps ** 2))
-    m_all = np.ones((axis_steps ** 2,))
-    kT_all = np.linspace(kT_min, kT_max, axis_steps)
-    mu_all = np.linspace(mu_min, mu_max, axis_steps)
-    cnt = 0
+def call_rabani_sweep(kT_mus, image_reps):
+    assert 0 not in kT_mus
+
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print(f"{current_time} - Beginning generation of {axis_res * axis_res * image_reps} rabanis")
+
+    for image_rep in range(image_reps):
+        imgs, m_all = run_rabani_sweep(kT_mus)
+        imgs = np.swapaxes(imgs, 0, 2)
+        save_rabanis(imgs, m_all, image_rep)
+
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        print(
+            f"{current_time} - Successfully completed block {image_rep + 1} of {image_reps} ({axis_res * axis_res} rabanis)")
+
+
+@jit(nopython=True, parallel=True, fastmath=True)
+def run_rabani_sweep(kT_mus):
+    axis_steps = len(kT_mus)
+    runs = np.zeros((128, 128, axis_steps))
+    m_all = np.zeros((axis_steps,))
 
     for i in prange(axis_steps):
-        cnt += 1
-        for j in prange(axis_steps):
-            mu = mu_all[i]
-            kT = kT_all[j]
-            runs[:, :, cnt + i + j - 1], m_all[cnt + i + j - 1] = rabani_single(kT, mu)
-            cnt += 1
+        runs[:, :, i], m_all[i] = rabani_single(kT_mus[i, 0], kT_mus[i, 1])
 
     return runs, m_all
+
+
+def make_storage_folder(date):
+    if os.path.isdir(f"{root_dir}/{date}/1") is False:
+        lastrun = 0
+    else:
+        allrun_str = list(filter(re.compile("[0-9]").match,
+                                 listdir(f"{root_dir}/{date}")))
+        lastrun = int(np.max(list(map(int, allrun_str))))
+
+    storage_folder = f"{root_dir}/{date}/{lastrun + 1}"
+    os.makedirs(storage_folder)
+
+    return storage_folder
+
+
+def save_rabanis(imgs, m_all, image_rep, savetype="txt"):
+    if savetype is "txt":
+        for rep, img in enumerate(imgs):
+            np.savetxt(
+                f"{save_dir}/rabani_kT={kT_mus[rep, 0]:.2f}_mu={kT_mus[rep, 1]:.2f}_nsteps={int(m_all[rep]):d}_rep={image_rep}.txt",
+                img, fmt="%01d")
+    elif savetype is "hdf5":
+        pass
 
 
 if __name__ == '__main__':
@@ -38,19 +73,13 @@ if __name__ == '__main__':
     start = time.time()
     root_dir = "Images"
     total_image_reps = 1
-    axis_res = 25
-    kT_range = [0, 6]
-    mu_range = [0, 6]
+    axis_res = 5
+    kT_range = [0.001, 2]
+    mu_range = [2, 4]
 
     # Make folders
     date = datetime.now().strftime("%Y-%m-%d")
-    if os.path.isdir(f"{root_dir}/{date}/1") is False:
-        lastrun = 0
-    else:
-        allrun_str = list(filter(re.compile("[0-9]").match,
-                                 listdir(f"{root_dir}/{date}")))
-        lastrun = int(np.max(list(map(int, allrun_str))))
-    os.makedirs(f"{root_dir}/{date}/{lastrun + 1}")
+    save_dir = make_storage_folder(date)
 
     # Shitty code to figure out order of pranges
     kT_mus = np.zeros((axis_res ** 2, 2))
@@ -60,21 +89,4 @@ if __name__ == '__main__':
             kT_mus[kT_mus_cnt, :] = [kT_val, mu_val]
             kT_mus_cnt += 1
 
-    # Do the thing!
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    print(f"{current_time} - Beginning generation of {axis_res * axis_res * total_image_reps} rabanis")
-
-    for image_rep in range(total_image_reps):
-        imgs, m_all = gen_rabani_sweep(kT_range[0], kT_range[1], mu_range[0], mu_range[1], axis_res)
-        imgs = np.swapaxes(imgs, 0, 2)
-
-        for rep, img in enumerate(imgs):
-            np.savetxt(
-                f"{root_dir}/{date}/{lastrun + 1}/rabani_kT={kT_mus[rep, 0]:.1f}_mu={kT_mus[rep, 1]:.1f}_nsteps{int(m_all[rep]):d}_{image_rep}.txt",
-                img, fmt="%01d")
-
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        print(
-            f"{current_time} - Successfully completed block {image_rep + 1} of {total_image_reps} ({axis_res * axis_res} rabanis)")
+    call_rabani_sweep(kT_mus, total_image_reps)

rabani.py
@@ -8,14 +8,16 @@ Rabani.m - Andrew Stannard 27/06/19, Rabani model in Matlab
 from math import exp
 from numba import jit
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import colors
 
 
-@jit(nopython=True)
+@jit(nopython=True, fastmath=True)
 def rabani_single(kT, mu):
     L = 128  # System length
     N = L ** 2  # System volume
 
-    MCS = 1000  # Max mc steps
+    MCS = 2000  # Max mc steps
     MR = 1  # Mobility ratio
     C = 0.30  # Nano-particle coverage
 
@@ -34,10 +36,7 @@ def rabani_single(kT, mu):
     liquid_array = np.abs(1 - nano_particles)
 
     # Set up checkpointing
-    I_tmp = np.random.choice(N, int(C * N), replace=False)
-    checkpoint_out = np.zeros((N,))
-    checkpoint_out[I_tmp] = 1
-    checkpoint_out = checkpoint_out.reshape((L, L))
+    checkpoint_out = np.ones((L, L))
     out = 2 * nano_particles + liquid_array
 
     perc_similarities = np.random.random((4,))
@@ -275,8 +274,15 @@ def rabani_single(kT, mu):
             # print(perc_similarities, perc_similarities_std)
             checkpoint_out = 2 * nano_particles + liquid_array
 
-        if 0 < perc_similarities_std < 0.005 and m < 100:
+        if 0 < perc_similarities_std < 0.01 and m > 100:
             break
 
     return out, m
 
+if __name__ == '__main__':
+    img, num_steps = rabani_single(kT=0.6, mu=3.8)
+
+    cmap = colors.ListedColormap(["black", "white", "orange"])
+    boundaries = [0, 0.5, 1]
+    norm = colors.BoundaryNorm(boundaries, cmap.N, clip=True)
+    plt.imshow(img, cmap=cmap)