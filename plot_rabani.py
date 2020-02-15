b1a06154de6efdb7e432b6cc1aee4bdb097e0ed7
Oliver Gordon <oliver.gordon@nottingham.ac.uk>
2020-02-15 14:58:12 +0000

    Smooth contours

--- plot_rabani.py
+++ plot_rabani.py
@@ -6,8 +6,9 @@ import numpy as np
 from matplotlib import colors
 from matplotlib.ticker import MultipleLocator
 from skimage import measure
+from skimage.filters import gaussian
 
-root_dir = "/home/mltest1/tmp/pycharm_project_883/Images/2020-02-14/21-16"
+root_dir = "/home/mltest1/tmp/pycharm_project_883/Images/2020-02-15/13-56"
 files = os.listdir(root_dir)
 
 # Find image details
@@ -30,6 +31,7 @@ big_img_arr = np.zeros((img_res * axis_res, img_res * axis_res))
 kT_vals = np.linspace(kT_range[0], kT_range[1], axis_res)
 mu_vals = np.linspace(mu_range[0], mu_range[1], axis_res)
 eulers = np.zeros((axis_res, axis_res))
+
 for i, file in enumerate(files):
     img_file = h5py.File(f"{root_dir}/{file}", "r")
 
@@ -46,11 +48,17 @@ cmap = colors.ListedColormap(["black", "white", "orange"])
 boundaries = [0, 0.5, 1]
 norm = colors.BoundaryNorm(boundaries, cmap.N, clip=True)
 
+target_axis_labels = 15
+num_tick_skip = len(mu_vals)//target_axis_labels
+mu_labels = [f"{mu_val:.2f}" for mu_val in mu_vals]
+kT_labels = [f"{kT_val:.2f}" for kT_val in kT_vals]
+
+# Sample grid
 fig1, ax1 = plt.subplots()
 plt.imshow(big_img_arr, cmap=cmap, origin="lower")
 
-plt.xticks(np.arange(len(mu_vals)) * img_res + img_res / 2, [f"{mu_val:.2f}" for mu_val in mu_vals], rotation=90)
-plt.yticks(np.arange(len(kT_vals)) * img_res + img_res / 2, [f"{kT_val:.2f}" for kT_val in kT_vals])
+plt.xticks(np.arange(len(mu_vals)) * img_res + img_res / 2, mu_labels, rotation=90)
+plt.yticks(np.arange(len(kT_vals)) * img_res + img_res / 2, kT_labels)
 ax1.set_xticks([x * img_res for x in range(axis_res)], minor=True)
 ax1.set_yticks([y * img_res for y in range(axis_res)], minor=True)
 
@@ -62,8 +70,9 @@ plt.grid(which="minor", ls="-", lw=2, color="r")
 # Euler
 fig2, ax2 = plt.subplots()
 cax2 = ax2.matshow(eulers, origin="lower", cmap="jet")
-X, Y = np.meshgrid(np.arange(len(eulers)), np.arange(len(eulers)))
-cnts2 = plt.contour(X, Y, eulers, colors="w", linestyles="solid")
+
+X2, Y2 = np.meshgrid(np.arange(len(eulers)), np.arange(len(eulers)))
+cnts2 = plt.contour(X2, Y2, gaussian(eulers, 1), colors="w", linestyles="solid")
 
 # Remove small contour circles
 for level in cnts2.collections:
@@ -74,10 +83,9 @@ for level in cnts2.collections:
             del (level.get_paths()[kp])
 
 # Pretty up axis
-num_tick_skip = 3
 ax2.xaxis.tick_bottom()
-ax2.set_xticklabels([''] + [f"{mu_val:.2f}" for mu_val in mu_vals][::num_tick_skip], rotation=90)
-ax2.set_yticklabels([''] + [f"{kT_val:.2f}" for kT_val in kT_vals][::num_tick_skip])
+ax2.set_xticklabels([''] + mu_labels[::num_tick_skip], rotation=90)
+ax2.set_yticklabels([''] + kT_labels[::num_tick_skip])
 ax2.xaxis.set_major_locator(MultipleLocator(num_tick_skip))
 ax2.yaxis.set_major_locator(MultipleLocator(num_tick_skip))
 
