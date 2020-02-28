eb9b7fc7b0f740dad7a3332929c74e1d35788056
Oliver Gordon <oliver.gordon@nottingham.ac.uk>
2020-02-28 13:49:28 +0000

    Generate with different lengthscales to allow for size invariance

--- gen_rabanis.py
+++ gen_rabanis.py
@@ -42,37 +42,45 @@ class RabaniSweeper:
         self.sftp = self.ssh.open_sftp()
 
     def call_rabani_sweep(self, params, axis_steps, image_reps):
-        assert 0 not in kT_range, "Setting any value to 0 will cause buffer overflows and corrupted runs!"
-
-        def get_linspace_ranges(ranges, axis_res):
-            if type(ranges) is list:
-                linspace = np.linspace(ranges[0], ranges[1], axis_res)
+        def get_linspace_ranges(param, param_key, axis_res):
+            if type(param[param_key]) is list:
+                if type(axis_res) is dict:
+                    if param_key in axis_res.keys():
+                        linspace = np.linspace(param[param_key][0], param[param_key][1], axis_res[param_key])
+                    else:
+                        linspace = [param[param_key]]
+                else:
+                    linspace = np.linspace(param[param_key][0], param[param_key][1], axis_res)
             else:
-                linspace = [ranges]
+                linspace = [param[param_key]]
             return linspace
 
-        kT_linspace = get_linspace_ranges(params["kT"], axis_steps)
-        mu_linspace = get_linspace_ranges(params["mu"], axis_steps)
-        MR_linspace = get_linspace_ranges(params["MR"], axis_steps)
-        C_linspace = get_linspace_ranges(params["C"], axis_steps)
-        e_nl_linspace = get_linspace_ranges(params["e_nl"], axis_steps)
-        e_nn_linspace = get_linspace_ranges(params["e_nn"], axis_steps)
-        L = get_linspace_ranges(params["L"], axis_steps)
+        kT_linspace = get_linspace_ranges(params, "kT", axis_steps)
+        mu_linspace = get_linspace_ranges(params, "mu", axis_steps)
+        MR_linspace = get_linspace_ranges(params, "MR", axis_steps)
+        C_linspace = get_linspace_ranges(params, "C", axis_steps)
+        e_nl_linspace = get_linspace_ranges(params, "e_nl", axis_steps)
+        e_nn_linspace = get_linspace_ranges(params, "e_nn", axis_steps)
+        L_all = get_linspace_ranges(params, "L", axis_steps)
 
+        tot_len = len(np.array(
+            list(product(kT_linspace, mu_linspace, MR_linspace, C_linspace, e_nl_linspace, e_nn_linspace, L_all))))
         current_time = self.start_datetime.strftime("%H:%M:%S")
-        print(f"{current_time} - Beginning generation of {axis_res * axis_res * image_reps} rabanis")
+        print(f"{current_time} - Beginning generation of {tot_len * image_reps} rabanis")
 
-        self.params = np.array(
-            list(product(kT_linspace, mu_linspace, MR_linspace, C_linspace, e_nl_linspace, e_nn_linspace, L)))
+        for L in L_all:
+            self.params = np.array(
+                list(product(kT_linspace, mu_linspace, MR_linspace, C_linspace, e_nl_linspace, e_nn_linspace, [L])))
+            assert 0. not in self.params, "Setting any value to 0 will cause buffer overflows and corrupted runs!"
 
-        for image_rep in range(image_reps):
-            imgs, m_all = _run_rabani_sweep(self.params)
-            imgs = np.swapaxes(imgs, 0, 2)
-            self.save_rabanis(imgs, m_all)
+            for image_rep in range(image_reps):
+                imgs, m_all = _run_rabani_sweep(self.params)
+                imgs = np.swapaxes(imgs, 0, 2)
+                self.save_rabanis(imgs, m_all)
 
-            now = datetime.now().strftime("%H:%M:%S")
-            print(
-                f"{now} - Successfully completed block {image_rep + 1} of {image_reps} ({axis_res * axis_res} rabanis)")
+                now = datetime.now().strftime("%H:%M:%S")
+                print(
+                    f"{now} - Successfully completed a block ({len(self.params)} rabanis)")
 
         self.end_datetime = datetime.now()
 
@@ -83,6 +91,7 @@ class RabaniSweeper:
     def save_rabanis(self, imgs, m_all):
         self.make_storage_folder(f"{self.root_dir}/{self.start_date}/{self.start_time}")
         for rep, img in enumerate(imgs):
+            print(img)
             master_file = h5py.File(
                 f"{self.root_dir}/{self.start_date}/{self.start_time}/rabanis--{platform.node()}--{self.start_date}--{self.start_time}--{self.sweep_cnt}.h5",
                 "a")
@@ -104,15 +113,18 @@ class RabaniSweeper:
 
             region_props = sim_results.create_group("region_props")
             region_props.create_dataset("euler_number", data=region["euler_number"])
-            region_props.create_dataset("normalised_euler_number", data=region["euler_number"]/np.sum(img == 2))
+            region_props.create_dataset("normalised_euler_number", data=region["euler_number"] / np.sum(img == 2))
             region_props.create_dataset("perimeter", data=region["perimeter"], dtype="f")
             region_props.create_dataset("eccentricity", data=region["eccentricity"], dtype="f")
+
+            master_file.close()
 
             if cat is not "none":
                 if self.sftp_when_done:
                     self.network_rabanis()
             elif self.generate_mode is "make_dataset":
-                os.remove(f"{self.root_dir}/{self.start_date}/{self.start_time}/rabanis--{platform.node()}--{self.start_date}--{self.start_time}--{self.sweep_cnt}.h5")
+                os.remove(
+                    f"{self.root_dir}/{self.start_date}/{self.start_time}/rabanis--{platform.node()}--{self.start_date}--{self.start_time}--{self.sweep_cnt}.h5")
 
             self.sweep_cnt += 1
 
@@ -128,13 +140,13 @@ class RabaniSweeper:
             else:
                 # Liquid if dominant category is water (==1)
                 cat = "liquid"
-        elif -0.015 <= region["euler_number"]/np.sum(img == 2) <= 0:
+        elif -0.015 <= region["euler_number"] / np.sum(img == 2) <= 0:
             # Cell/Worm if starting to form
             cat = "cellular"
-        elif -0.03 <= region["euler_number"]/np.sum(img == 2) < -0.015:
+        elif -0.03 <= region["euler_number"] / np.sum(img == 2) < -0.015:
             # Labyrinth
             cat = "labyrinth"
-        elif region["euler_number"]/np.sum(img == 2) <= -0.05:
+        elif region["euler_number"] / np.sum(img == 2) <= -0.05:
             # Island
             cat = "island"
         else:
@@ -155,22 +167,20 @@ class RabaniSweeper:
 if __name__ == '__main__':
     root_dir = "Images"
     total_image_reps = 1
-    axis_res = 40
 
-    kT_range = [0.01, 0.35]
-    mu_range = [2.35, 3.47]
-    MR_range = 1
-    C_range = 0.3
-    e_nl_range = 1.5
-    e_nn_range = 2
-    L = 128
+    parameters = {"kT": [0.01, 0.35],
+                  "mu": [2.35, 3.47],
+                  "MR": 1,
+                  "C": 0.3,
+                  "e_nl": 1.5,
+                  "e_nn": 2,
+                  "L": [64, 128]}
+
+    axis_res = {"kT": 4,
+                "mu": 4,
+                "L": 2}
 
     rabani_sweeper = RabaniSweeper(root_dir=root_dir, generate_mode="make_dataset")
-    rabani_sweeper.call_rabani_sweep(params={"kT": kT_range,
-                                             "mu": mu_range,
-                                             "MR": MR_range,
-                                             "C": C_range,
-                                             "e_nl": e_nl_range,
-                                             "e_nn": e_nn_range,
-                                             "L": L},
-                                     axis_steps=axis_res, image_reps=total_image_reps)
+    rabani_sweeper.call_rabani_sweep(params=parameters,
+                                     axis_steps=axis_res,
+                                     image_reps=total_image_reps)
